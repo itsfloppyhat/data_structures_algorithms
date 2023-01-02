2118ce0913868187f436d4a9d743c38598c0144c
itsfloppyhat <itsfloppyhat@gmail.com>
2023-01-01 19:24:41 -0500

    added Validate Subsequence

---
+++ dsa_questions.playground/Pages/Validate Subsequence.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*
+ Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
+ 
+ A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that are in the same order as they appear in the array. For instance, the numbers [1,3,4] form a subsequence of the array [1,2,3,4], and so do the numbers [2,4]. Note that a single number in an array and the array itself are both valid subsequences of the array.
+ */
+
+import Foundation
+
+func isValidSubsequence(_ array: [Int], _ sequence: [Int]) -> Bool {
+    var sequenceCounter = 0
+    for i in 0..<array.count {
+      if array[i] == sequence[sequenceCounter] {
+        sequenceCounter += 1
+        if sequenceCounter == sequence.count {
+          return true
+        }
+      }
+    }
+    return false
+  }
+var array = [5,1,22,25,6,-1,8,10]
+var sequence = [1,6,-1,10]
+
+print(isValidSubsequence(array, sequence)) //equals true
+
